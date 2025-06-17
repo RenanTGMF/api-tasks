f4f216c5048e9ade9e0b5bc6e349f43947d69a7a
RenanTGMF <renan_mariano20@hotmail.com>
2025-06-17 16:38:41 -0300

    feat: add database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import http from 'node:http';
+import { Database } from './database.js';
 
 const server = http.createServer((req, res) => {
+    const database = new Database()
+
     return res.writeHead(404).end()
 })
 
