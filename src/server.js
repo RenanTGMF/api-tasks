835a96a159d9868998e90ee837b6540b9fafd693
RenanTGMF <renan_mariano20@hotmail.com>
2025-06-17 18:51:48 -0300

    feat: add JSON body parser middleware

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import http from 'node:http';
 import { Database } from './database.js';
+import { json } from './middlewares/json.js';
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const database = new Database()
+
+    await json(req, res)
 
     return res.writeHead(404).end()
 })
